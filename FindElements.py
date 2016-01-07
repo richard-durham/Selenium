c76d2eefdde7643299a1272464dbb1afb9eb96b5
Richard Durham <richard_durham@me.com>
2016-01-07 16:45:00 +0000

    feat: Move code to git and GitHub

---
+++ FindElements.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by  import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+import unittest
+
+
+class FindElements(unittest.TestCase):
+
+    def setUp(self):
+        global driver
+        driver = webdriver.Firefox()
+        driver.get("http://travelingtony.weebly.com/store/c2/Lope.html")
+        driver.implicitly_wait(15)
+    
+    
+    def test_FindElements(self):
+        #webElementsList      = driver.find_elements_by_xpath("//img")
+        webElementsList      = driver.find_elements_by_css_selector("img")
+        #webElementsList      = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_xpath(""))
+        #webElementsList      = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_css_selector(""))
+        print webElementsList
+        print "# of images on the page =", len(webElementsList)
+    
+    
+    def tearDown(self):
+        driver.quit()
+
+if __name__ == "__main__":
+   unittest.main()
+
+
+
